54c7a7d6aa455f5aab0add16cdefa41fba5c7ea7
Siarhei_Wehrhahn <sergei1997.sw@gmail.com>
2024-03-21 21:05:01 +0100

    Fertig

--- JRadio/JRadioApp.swift
+++ JRadio/JRadioApp.swift
@@ -29,7 +29,7 @@ struct JRadioApp: App {
                             }
                         }
                 } else {
-                    InfoTextView()
+                    ContentView()
                         .preferredColorScheme(.light)
                 }
             }
